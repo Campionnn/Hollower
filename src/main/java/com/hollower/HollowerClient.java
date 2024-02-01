31d9cc801e6ab84661109c236b2cb76c4dce43ba
Campion <ryanchien2468@gmail.com>
2024-01-31 23:49:42 -0800

    initial commit

---
+++ src/main/java/com/hollower/HollowerClient.java
@@ -0,0 +1,10 @@
+package com.hollower;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HollowerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/hollower/HollowerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hollower;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HollowerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
