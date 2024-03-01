a2fb5b3c96609f98486f2c1e6ca8d4a3f200de78
Campion <ryanchien2468@gmail.com>
2024-03-01 03:24:13 -0800

    many changes

--- src/main/java/com/hollower/Hollower.java
+++ src/main/java/com/hollower/Hollower.java
@@ -1,20 +1,66 @@
 package com.hollower;
 
+import com.hollower.utils.PlayerUtils;
+import com.hollower.utils.RouteUtils;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hollower.utils.RenderUtils;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class Hollower implements ModInitializer {
-	public static final String MODID = "hollower";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final String MOD_ID = "hollower";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static MinecraftClient client = MinecraftClient.getInstance();
+	public static long lastToolUseTick;
+	public static List<BlockPos> positions = new ArrayList<>();
+	public static int maxReach = 20;
+	public static BlockPos selected;
+	public static int nudgeKey = GLFW.GLFW_KEY_LEFT_CONTROL;
+	public static int swapOrderKey = GLFW.GLFW_KEY_LEFT_ALT;
+	public static int etherwarpKey = GLFW.GLFW_KEY_LEFT_SHIFT;
+	public static Map<Integer, Boolean> keys = new HashMap<>();
+	static {
+		keys.put(nudgeKey, false);
+		keys.put(swapOrderKey, false);
+		keys.put(etherwarpKey, false);
+
+	}
 
 	@Override
+//	@Environment(EnvType.CLIENT)
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		AttackBlockCallback.EVENT.register(new PlayerUtils());
 
-		LOGGER.info("Hello Fabric world!");
+		WorldRenderEvents.LAST.register((context) -> {
+			RenderUtils.drawLines(positions, Color.RED, 3.0F, false);
+			RenderUtils.highlightBlocks(positions, Color.GREEN, 2.0f, false);
+			RenderUtils.selectBlock(selected, new Color(0, 0, 255, 64), false);
+			if (keys.get(etherwarpKey) && PlayerUtils.isHoldingTool()) {
+				BlockPos etherwarpPos = RouteUtils.getRaycast(61);
+				RenderUtils.selectBlock(etherwarpPos, new Color(255, 0, 255, 64), false);
+			}
+
+			RenderUtils.renderOrder(positions);
+		});
+	}
+
+	public static void onKeyEvent(int key, int scancode, int action, int modifiers) {
+		if (keys.containsKey(key)) {
+			keys.put(key, action != GLFW.GLFW_RELEASE);
+		}
 	}
 }
