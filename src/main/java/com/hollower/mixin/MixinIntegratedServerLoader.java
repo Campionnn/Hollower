58e82765fdeb9602882f40026a178f308a504c95
Campion <ryanchien2468@gmail.com>
2024-03-01 23:11:14 -0800

    mixin to ignore experimental settings warning when opening clean ch world

---
+++ src/main/java/com/hollower/mixin/MixinIntegratedServerLoader.java
@@ -0,0 +1,23 @@
+package com.hollower.mixin;
+
+import net.minecraft.server.integrated.IntegratedServerLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+/**
+ * bypasses the warning message when loading clean crystal hollows world
+ */
+@Mixin(IntegratedServerLoader.class)
+public class MixinIntegratedServerLoader {
+    @ModifyVariable(method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V", at = @At("HEAD"), argsOnly = true, index = 4)
+    private boolean removeAdviceOnLoad(boolean original) {
+        return false;
+    }
+
+    // Set bypassWarnings = true
+    @ModifyVariable(method = "tryLoad", at = @At("HEAD"), argsOnly = true, index = 4)
+    private static boolean removeAdviceOnCreation(boolean original) {
+        return true;
+    }
+}
