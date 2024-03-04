fa0a04ee839227abe53223fd69154e1525fac83c
Campion <ryanchien2468@gmail.com>
2024-03-03 23:17:20 -0800

    mixin to remove fog

---
+++ src/main/java/com/hollower/mixin/MixinBackgroundRenderer.java
@@ -0,0 +1,20 @@
+package com.hollower.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.FogShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BackgroundRenderer.class)
+public class MixinBackgroundRenderer {
+    @Inject(method = "applyFog", at = @At("RETURN"))
+    private static void applyFogModifyDistance(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
+        RenderSystem.setShaderFogStart(Float.MAX_VALUE);
+        RenderSystem.setShaderFogEnd(Float.MAX_VALUE);
+        RenderSystem.setShaderFogShape(FogShape.CYLINDER);
+    }
+}
