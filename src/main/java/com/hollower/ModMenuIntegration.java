571166f213158ae022983bd7496df4ffb60d389e
Campion <ryanchien2468@gmail.com>
2024-03-05 03:11:48 -0800

    integration with mod menu

---
+++ src/main/java/com/hollower/ModMenuIntegration.java
@@ -0,0 +1,11 @@
+package com.hollower;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> Hollower.createConfigBuilder().build();
+    }
+}
